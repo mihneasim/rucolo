f486c5228d69a92acb2e979e6434a561444eb643
Mihnea Simian <contact@mesimian.com>
2016-01-01 12:29:29 +0200

    bootstrapping a2

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>Rucolo</h1>
+    <p>Drag and match the colors</p>
+    `
+})
+export class AppComponent { };
