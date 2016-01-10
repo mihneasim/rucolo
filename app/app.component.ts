2cf08e77e724edd73bf5b908eb183474139e3e18
Mihnea Simian <contact@mesimian.com>
2016-01-10 19:34:18 +0200

    some basic absolute positioning to begin with

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,14 +7,20 @@ import {BoardService,Colors} from './board.service';
     <h1>Rucolo</h1>
     <p>Drag and match the colors</p>
 
-    <div *ngFor="#cell of cells">
-        {{cell}}
+    <div class="canvas">
+        <div *ngFor="#cell of cells; #i = index"
+            id="cell{{i}}" [ngClass]="['cell', 'row' + floor(i/3), 'col' + i%3]">
+            {{cell}}
+        </div>
     </div>
+    <a href="#">Shift </a>
     `,
+    styleUrls: ['app/app.styles.css'],
     providers: [BoardService]
 })
 export class AppComponent implements OnInit {
     public cells: Colors[];
+    public floor = Math.floor;
 
     constructor(private _boardService: BoardService) { }
 
